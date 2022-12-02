1dc982457d80fba09b85da8c5b41a95ca8aa0717
Brigham Brown <me@brighambrown.com>
2022-12-01 20:06:46 -0800

    Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
+    fun solve(input: List<String>, n: Int = 1): List<Int> {
+        val calHeap = descendingHeap<Int>()
+        input.asSequence().chunkedBy { it.isNotBlank() }
+            .map { it.map { it.toInt() }.sum() }
+            .forEach { calHeap.add(it) }
+
+        return (0 until n).map { calHeap.remove() }.toList()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return solve(input)[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return solve(input, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 25000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,7 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.PriorityQueue
 
 /**
  * Reads lines from the given input txt file.
@@ -14,3 +15,36 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun <E> Sequence<E>.chunkedBy(predicate: (E) -> Boolean): Sequence<List<E>> {
+    val outer = this
+    var justYielded = false
+    return sequence {
+        val list = mutableListOf<E>()
+        for (item in outer) {
+            justYielded = false
+            if (predicate(item)) {
+                list.add(item)
+            } else {
+                yield(list.toList())
+                list.clear()
+                justYielded = true
+            }
+        }
+        if (!justYielded) {
+            yield(list.toList())
+        }
+    }
+}
+
+fun <E : Comparable<E>> heap(): PriorityQueue<E> {
+    return PriorityQueue<E>()
+}
+
+fun <E : Comparable<E>> descendingHeap(): PriorityQueue<E> {
+    return heap(naturalOrder<E>().reversed())
+}
+
+fun <E> heap(comparator: Comparator<E>): PriorityQueue<E> {
+    return PriorityQueue<E>(comparator)
+}
